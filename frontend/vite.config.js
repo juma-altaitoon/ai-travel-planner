2a012c8b2c852edb0adb49f3ec7b1a5a07cfd7d5
Juma <juma.altaitoon@gmail.com>
2025-08-03 12:29:59 +0300

    deployment fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // server: {
-  //   allowedHosts: [ 'http://localhost' ]
-  // },
   plugins: [react()],
-  base: '/ai-travel-planner/frontend/',
+  
 })
